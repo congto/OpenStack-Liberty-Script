b4e1fe3dbb1c5c3d59c2fb64180b4c464eecdf3e
Tô Thành Công <tcvn1985@gmail.com>
2016-03-13 18:13:42 +0700

    set name file
    set name file

--- LIBERTY-U14.04-OVS/com1-ceilomter.sh
+++ LIBERTY-U14.04-OVS/com1-ceilomter.sh
@@ -4,11 +4,14 @@
 source config.cfg
 
 # Cong cu de sua file cau hinh
-apt-get -y install  crudini 
+apt-get -y install python-pip
+pip install https://pypi.python.org/packages/source/c/crudini/crudini-0.7.tar.gz
 
 ####################################   
 # Ceilomter agent for Compute node #
 ####################################
+
+
 
 echo "Installing Ceilomter agent for Compute node"
 sleep 3
@@ -24,42 +27,43 @@ sleep 3
 
 # Edit [DEFAULT] section 
 
-crudini --set /etc/ceilometer/ceilometer.conf DEFAULT rpc_backend rabbit
-crudini --set /etc/ceilometer/ceilometer.conf DEFAULT auth_strategy keystone
-crudini --set /etc/ceilometer/ceilometer.conf DEFAULT verbose True
+crudini --set $fileceilomter DEFAULT rpc_backend rabbit
+crudini --set $fileceilomter DEFAULT auth_strategy keystone
+crudini --set $fileceilomter DEFAULT verbose True
 
 
 # Edit [slo_messaging_rabbit] section 
-crudini --set  /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_host $CON_MGNT_IP
-crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_userid openstack
-crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_password $RABBIT_PASS
+crudini --set  $fileceilomter oslo_messaging_rabbit rabbit_host $CON_MGNT_IP
+crudini --set $fileceilomter oslo_messaging_rabbit rabbit_userid openstack
+crudini --set $fileceilomter oslo_messaging_rabbit rabbit_password $RABBIT_PASS
 
 # Edit [keystone_authtoken] section 
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_uri http://$CON_MGNT_IP:5000
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_url http://$CON_MGNT_IP:35357
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_plugin password
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken project_domain_id default
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken user_domain_id default
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken project_name service
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken username ceilometer
-crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken password $CEILOMETER_PASS
+crudini --set $fileceilomter keystone_authtoken auth_uri http://$CON_MGNT_IP:5000
+crudini --set $fileceilomter keystone_authtoken auth_url http://$CON_MGNT_IP:35357
+crudini --set $fileceilomter keystone_authtoken auth_plugin password
+crudini --set $fileceilomter keystone_authtoken project_domain_id default
+crudini --set $fileceilomter keystone_authtoken user_domain_id default
+crudini --set $fileceilomter keystone_authtoken project_name service
+crudini --set $fileceilomter keystone_authtoken username ceilometer
+crudini --set $fileceilomter keystone_authtoken password $CEILOMETER_PASS
 
 # Edit [service_credentials] section 
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_auth_url http://$CON_MGNT_IP:5000/v2.0
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_username ceilometer
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_tenant_name service
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_password $CEILOMETER_PASS
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_endpoint_type internalURL
-crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_region_name RegionOne	
+crudini --set $fileceilomter service_credentials os_auth_url http://$CON_MGNT_IP:5000/v2.0
+crudini --set $fileceilomter service_credentials os_username ceilometer
+crudini --set $fileceilomter service_credentials os_tenant_name service
+crudini --set $fileceilomter service_credentials os_password $CEILOMETER_PASS
+crudini --set $fileceilomter service_credentials os_endpoint_type internalURL
+crudini --set $fileceilomter service_credentials os_region_name RegionOne	
 
 echo "Edit file /etc/nova/nova.conf on Compute node"
 sleep 3
 
 # Edit [DEFAULT] section 
-crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit True
-crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit_period hour
-crudini --set /etc/nova/nova.conf DEFAULT notify_on_state_change vm_and_task_state
-crudini --set /etc/nova/nova.conf DEFAULT notification_driver messagingv2
+filenova=/etc/nova/nova.conf
+crudini --set $filenova DEFAULT instance_usage_audit True
+crudini --set $filenova DEFAULT instance_usage_audit_period hour
+crudini --set $filenova DEFAULT notify_on_state_change vm_and_task_state
+crudini --set $filenova DEFAULT notification_driver messagingv2
 
 echo "Restart ceilometer-agent-compute, nova-compute"
 sleep 3
