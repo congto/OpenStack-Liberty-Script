5a10ef7161157389774f96f60c93aea354688075
Tô Thành Công <tcvn1985@gmail.com>
2016-05-03 16:54:42 +0700

    update
    update

--- LIBERTY-U14.04-OVS/ctl-6-neutron.sh
+++ LIBERTY-U14.04-OVS/ctl-6-neutron.sh
@@ -197,6 +197,8 @@ ops_del $netmetadata DEFAULT admin_password
 su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
   --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
   
+
+  
 echocolor "Restarting NOVA service"
 sleep 7 
 service nova-api restart
@@ -206,7 +208,7 @@ service nova-conductor restart
 echocolor "Restarting NEUTRON service"
 sleep 7 
 service neutron-server restart
-service neutron-plugin-openvswitch-agent restart
+service neutron-openvswitch-agent restart
 service neutron-dhcp-agent restart
 service neutron-metadata-agent restart
 service neutron-l3-agent restart
